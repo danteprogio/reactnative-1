7e099affc3ad22e9f3629723b9059546fce586dd
danteprogio <potpotprogio5@gmail.com>
2024-05-24 16:25:59 +0800

    added scanner camera

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,12 +7,11 @@ import 'react-native-reanimated';
 import { todoListData } from '@/components/Context/TodoContext';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [todoLists,setTodoLists] = useState([])
+  const [todoLists,setTodoLists] = useState([{"check": true, "id": "7ef99745-3546-4c7c-8f43-2df8344b5051", "status": "Complete", "title": "Create a Todo task"},{"check": false, "id": "1f357cb5-4f65-474b-9e40-96611b89280a", "status": "Pending", "title": "Create a PokeDex"}])
 
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
@@ -31,12 +30,10 @@ export default function RootLayout() {
     return null;
   }
 
- 
   return (
       <todoListData.Provider value={{setTodoLists, todoLists}}>
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
           <Stack>
-            <Stack.Screen name="camera" options={{ headerShown: false }} />
             <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen
--- app/index.tsx
+++ app/index.tsx
@@ -1,65 +1,88 @@
-import { CameraView, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { router } from 'expo-router';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity ,StyleSheet} from 'react-native';
+import { ALERT_TYPE, Dialog, AlertNotificationRoot, Toast } from 'react-native-alert-notification';
+const LoginView = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default function App() {
-  const [facing, setFacing] = useState('back');
-  const [permission, requestPermission] = useCameraPermissions();
+  const handleLoginPress = () => {
+    if ( password === 'admin' && email === 'Admin' ){
+        router.push('(tabs)')
+    }else{
+      Toast.show({
+        type: ALERT_TYPE.DANGER,
+        title: 'Wrong Password or Email',
+        textBody: 'Kindly double check your Password or Email',
+      })
+    }
 
-  if (!permission) {
-    // Camera permissions are still loading.
-    return <View />;
-  }
-
-  if (!permission.granted) {
-    // Camera permissions are not granted yet.
-    return (
-      <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
-  }
-
-  function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
-  }
+  };
 
   return (
-    <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
+    <AlertNotificationRoot>
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <Text style={styles.title}>Login</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            onChangeText={text => setPassword(text)}
+            secureTextEntry
+          />
+          <TouchableOpacity style={styles.button} onPress={handleLoginPress}>
+            <Text style={styles.buttonText}>Log In</Text>
           </TouchableOpacity>
         </View>
-      </CameraView>
-    </View>
+      </View>
+    </AlertNotificationRoot>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
     justifyContent: 'center',
   },
-  camera: {
-    flex: 1,
+  card: {
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    borderRadius: 10,
+    padding: 20,
+    width: '80%',
   },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
+  title: {
+    fontSize: 30,
+    marginBottom: 5,
+    fontFamily: "Scripto2OR2v",
+  },
+  input: {
+    borderBottomColor: '#999',
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+    width: '90%',
   },
   button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+    backgroundColor: '#0066cc',
+    borderRadius: 5,
+    padding: 10,
+    width: '90%',
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
+
+export default LoginView;
